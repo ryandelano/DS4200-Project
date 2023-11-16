1802186243267e58f426553cfa71e87d0d55ead4
Ryan Delano <rdelano6@gmail.com>
2023-11-16 16:34:36 -0500

    first draft of getting data from CCC api

---
+++ api_pull.py
@@ -0,0 +1,79 @@
+import json
+
+import pandas as pd
+import requests
+import random as rnd
+import time
+import json
+import io
+
+url_dict = {
+    "approved_apps" : "https://masscannabiscontrol.com/resource/hmwt-yiqy",
+    "pending_apps" : "https://masscannabiscontrol.com/resource/piib-tj3f",
+    "dbe_apps" : "https://masscannabiscontrol.com/resource/5dkg-e39p",
+    "apro_apps" : "https://masscannabiscontrol.com/resource/albs-all",
+    "total_apps" : "https://masscannabiscontrol.com/resource/n6qz-us6r",
+    "agent_gender" : "https://masscannabiscontrol.com/resource/hhjg-atjk",
+    "agent_race" : "https://masscannabiscontrol.com/resource/pt2c-wb44",
+    "retdel_sales" : "https://masscannabiscontrol.com/resource/87rp-xn9v",
+    "retdel_weekly" : "https://masscannabiscontrol.com/resource/dt9b-i6ds",
+    "monthly_oz_price" : "https://masscannabiscontrol.com/resource/rqtv-uenj",
+    "medical_stats" : "https://masscannabiscontrol.com/resource/g5mj-5pg3",
+    "plant_activity" : "https://masscannabiscontrol.com/resource/meau-plav",
+    "facility_sales" : "https://masscannabiscontrol.com/resource/fren-z7jq"
+}
+
+user_agents = [
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"
+]
+
+header = {
+    'User-Agent': rnd.choice(user_agents),
+    'Accept-Language': 'en-US, en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,*/*;q=0.7,*/*;q=0.5',
+    'Referer': 'https://www.mass.gov',
+    'DNT': '1'
+}
+
+df_dict = {}
+
+for title, url in url_dict.items():
+    print(title)
+    for _ in range(5):  # Retry up to 5 times
+        try:
+            with requests.get(url + ".json", headers=header, stream=False) as response:
+                content = response.content.decode('utf-8-sig')  # Decode using 'utf-8-sig'
+                data = json.loads(content)  # Parse the decoded content as JSON
+                df = pd.DataFrame(data)
+                df_dict[title] = df
+                print("SUCCESSFUL REQUEST: JSON")
+                break  # If the request was successful, break the loop
+        except Exception as error:
+            print(error)
+            print("testing csv...")
+            time.sleep(2)
+            try:
+                with requests.get(url + ".csv", headers=header, stream=False) as response:
+                    content = response.content.decode('utf-8-sig') # Decode using 'utf-8-sig'
+                    data = io.StringIO(content)  # Parse the decoded content as csv
+                    df = pd.read_csv(data)
+                    df_dict[title] = df
+                    print("SUCCESSFUL REQUEST: CSV")
+                    break  # If the request was successful, break the loop
+            except Exception as error:
+                print(error)
+                print("ChunkedEncodingError occurred, retrying...")
+                time.sleep(5)
+
+for title, df in df_dict.items():
+    print(title)
+    print(df.head())
+    print(df.shape)
+    print("\n")
